bbc5fcf14054f6644ccb1546e4dcf0bd68f4f40e
Edward Viaene <ward.viaene@gmail.com>
2017-05-08 08:18:11 +0000

    nodejs job dsl example

---
+++ job-dsl/nodejs.groovy
@@ -0,0 +1,18 @@
+job('NodeJS example') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
