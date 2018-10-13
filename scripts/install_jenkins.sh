4257d220fcb325e8090540eb8c39e28b6dec11ee
Mike S <aware74@users.noreply.github.com>
2018-10-13 15:56:44 -0400

    Point to jenkins/jenkins:lts image within DockerHub
    If we don't point to jenkins/jenkins:lts image, than subsequent plugin updates will fail when attempting to install recommended plugins.

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -14,7 +14,7 @@ usermod -aG docker ubuntu
 # run jenkins
 mkdir -p /var/jenkins_home
 chown -R 1000:1000 /var/jenkins_home/
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins
+docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
 
 # show endpoint
 echo 'Jenkins installed'
