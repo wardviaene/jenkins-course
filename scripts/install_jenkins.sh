2e2eceb404eae13afb0879271e85519ed86af8c1
Edward Viaene <ward@in4it.io>
2020-04-26 13:49:51 +0200

    update install script to remove old URLs

--- scripts/install_jenkins.sh
+++ scripts/install_jenkins.sh
@@ -3,10 +3,13 @@
 # this script is only tested on ubuntu xenial
 
 # install docker
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 apt-get update
-apt-get install -y docker-engine
+apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
 systemctl start docker
 usermod -aG docker ubuntu
