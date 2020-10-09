74d7000c9fe06c13c17db93e14c8acf00cce5e37
biswa123642 <64695542+biswa123642@users.noreply.github.com>
2020-10-09 18:04:48 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/biswa123642/jenkins-docker.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('biswajitsahu357@gmail.com')
         }
     }
     triggers {
