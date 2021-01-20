d2ebcfa8c74127d74c4d282821f2d35463335ba6
csenguttuvan <chris.senguttuvan@kaltura.com>
2021-01-20 17:44:01 +0000

    Changed groovy job name & git repo

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
-job('NodeJS example') {
+job('Groovy-edit1') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/csenguttuvan/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('csenguttuvan')
+            node / gitConfigEmail('chris.senguttuvan@kaltura.com')
         }
     }
     triggers {
