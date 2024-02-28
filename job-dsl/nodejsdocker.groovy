e640d1e7ed2864fdb2570b271833790254621a56
Ivan Fabriano Syahputra <78840337+ivanfabriano@users.noreply.github.com>
2024-02-28 13:39:15 +0700

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('ivanfabs/docker-example')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
